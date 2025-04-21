304a5394b96d383be5474fe65efda59f956c91f3
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-21 23:44:59 +0300

    creating tasks object and puching it to user

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let user = JSON.parse(localStorage.getItem("curUser")) || [];
+    let curUser = JSON.parse(localStorage.getItem("curUser")) || [];
     headerUser = document.getElementById("header-username");
-    headerUser.textContent = user.username;
+    headerUser.textContent = curUser.username;
+
+    addTaskBtn = document.getElementById("addTask");
+    addTaskBtn.addEventListener("click", () => {
+        taskTitle = document.getElementById("task-input-title").value;
+        taskDate = document.getElementById("task-input-date").value;
+
+        tasks = {
+            taskTitle: taskTitle,
+            taskDate: taskDate
+        };
+
+        curUser.tasks.push(tasks);
+
+        //console.log(curUser);
+
+        ul = document.getElementById("task-list");
+        li = document.createElement("li");
+        li.textContent = `${tasks.taskTitle} At ${tasks.taskDate}`;
+        li.classList.add("list-item");
+        ul.appendChild(li);
+    })
 });
