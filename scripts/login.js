c346cb5760eeed603fcce9c334d7f7606252994b
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-21 20:31:31 +0300

    Getting user input from login and regUsers from localstorage

--- scripts/login.js
+++ scripts/login.js
@@ -4,5 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
         const userEmail = document.getElementById("email").value;
         const userPassword = document.getElementById("password").value;
         //console.log(`User ${userEmail}: ${userPassword}`);
-    })
-})
+
+        let regUsers = JSON.parse(localStorage.getItem("users")) || [];
+        console.log(regUsers);
+
+        const matchedUser = regUsers.find(user => (
+            user.email === userEmail && user.password === userPassword
+        ));
+
+        if (matchedUser) {
+            //console.log(matchedUser);
+        } else {
+            //console.log("Not found");
+        }
+    });
+});
